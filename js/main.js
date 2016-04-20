1a095fea90586637c3bd446057f1e0dd44e8c11f
jjchambl <jjchambliss@gmail.com>
2016-04-19 23:33:36 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var app = angular.module('brewApp', ['brewApp.controllers', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/welcome.html',
+        controller: 'WelcomeCtrl'
+    })
+    .when('/goodppl', {
+        templateUrl: 'views/goodppl.html',
+        controller: 'GoodCtrl'
+    })
+    .when('/cahaba', {
+        templateUrl: 'views/cahaba.html',
+        controller: 'CahabaCtrl'
+    })
+    .when('/trimtab', {
+        templateUrl: 'views/trimtab.html',
+        controller: 'TrimTabCtrl'
+    })
+    .when('/avondale', {
+        templateUrl: 'views/avondale.html',
+        controller: 'AvondaleCtrl'
+    })
+}])
