984da22266ae1171f47daed6e3b8276837c40522
jjchambl <jjchambliss@gmail.com>
2016-04-25 23:43:22 -0500

    factory, http request complete

--- js/controllers/welcomeCtrl.js
+++ js/controllers/welcomeCtrl.js
@@ -2,33 +2,23 @@ angular.module("brewApp")
 
 .controller("WelcomeCtrl", WelcomeController);
 
-WelcomeController.$inject = ["$scope", "$http", 'brewFactory'];
+WelcomeController.$inject = ["$scope", "$http", 'goodFactory'];
 
-function WelcomeController($scope, $http, brewFactory) {
-    var sayHello = function() {
-        console.log("hello!");
-    }
+function WelcomeController($scope, $http, goodFactory) {
     
-    // $scope.beers = brewFactory.goodppl;
+    $scope.beers = [];
     
     var getBrews = function() {
         $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
             console.log(data.response.brewery.beer_list.items);
             var beerList = data.response.brewery.beer_list.items;
+            $scope.beers = beerList;
         });
-        
-        // brewFactory.getBrews().success(function(data) {
-        //     console.log(data);
-        // });
-        // console.log(brewFactory.getBrews());
-        // for (var i = 0; i < brewFactory.length; i++) {
-            
-        // }
-        // $scope.beers = brewFactory;        
-        // console.log(brewFactory.getBrews().data.response.brewery.beer_list.items);
-        // $scope.beers = brewFactory;
+        // This does not work on load, as it requires a promise that needs to resolve.
+        // goodFactory.getBrews();
+        // console.log(goodFactory.brews[0]);
+        // $scope.beers = goodFactory.brews[0];
     }
     
-    // $scope.init = brewFactory.goodppl = [];
     $scope.init = getBrews();
 }
--- js/factories.js
+++ js/factories.js
@@ -1,12 +1,45 @@
 var factories = angular.module('brewApp.factories', []);
 
-factories.factory('brewFactory', ['$http', function($http) {
+factories.factory('goodFactory', ['$http', function($http) {
     var f = {};
     
-    // f.goodppl = [];
+    f.brews = [];
     
     f.getBrews = function() {
         $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
+        // See welcomectrl.js.17 for the below handler.
+        // .success(function(data) {
+        //     f.brews.push(data.response.brewery.beer_list.items);
+        // }, function(err) {
+        //     console.log(err);
+        // });
     }
     return f;
-}])
+}]);
+
+factories.factory('cahabaFactory', ['$http', function($http) {
+    var f = {};
+    
+    f.getBrews = function() {
+        $http.get('https://api.untappd.com/v4/brewery/info/30313/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
+    }
+    return f;
+}]);
+
+factories.factory('trimFactory', ['$http', function($http) {
+    var f = {};
+    
+    f.getBrews = function() {
+        $http.get('https://api.untappd.com/v4/brewery/info/66716/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
+    }
+    return f;
+}]);
+
+factories.factory('avondaleFactory', ['$http', function($http) {
+    var f = {};
+    
+    f.getBrews = function() {
+        $http.get('https://api.untappd.com/v4/brewery/info/12339/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
+    }
+    return f;
+}]);
