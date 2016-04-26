08b62492938bbc4a2bbd06f948bfe155866ac4fe
Thomas Walker <thomasbwalker@gmail.com>
2016-04-26 14:01:41 -0500

    untappd panels and funstion on GP page

--- js/controllers/goodCtrl.js
+++ js/controllers/goodCtrl.js
@@ -4,15 +4,23 @@ angular.module("brewApp")
 .controller("GoodCtrl", GoodPeopleController);
 
 // Listing the dependencies
-GoodPeopleController.$inject = ["$scope", "NgMap"];
+GoodPeopleController.$inject = ["$scope", "NgMap", "$http"];
 
 // implementing the dependencies
-function GoodPeopleController($scope, NgMap) {
+function GoodPeopleController($scope, NgMap, $http) {
     $scope.message = "For the Good ones.";
     NgMap.getMap().then(function(map) {
     console.log(map.getCenter());
     console.log('markers', map.markers);
     console.log('shapes', map.shapes);
-  });
-        
+    });
+    
+    var getBrews = function() {
+        $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
+            console.log(data.response.brewery.beer_list.items);
+            var beerList = data.response.brewery.beer_list.items;
+            $scope.beers = beerList;
+        })
+    }
+    $scope.init = getBrews ();    
 }
