b125b7f1966d4b475ddd4758b49c02bdf97d78b2
jjchambl <jjchambliss@gmail.com>
2016-04-25 22:22:32 -0500

    api change

--- js/controllers/welcomeCtrl.js
+++ js/controllers/welcomeCtrl.js
@@ -2,9 +2,9 @@ angular.module("brewApp")
 
 .controller("WelcomeCtrl", WelcomeController);
 
-WelcomeController.$inject = ["$scope", 'brewFactory'];
+WelcomeController.$inject = ["$scope", "$http", 'brewFactory'];
 
-function WelcomeController($scope, brewFactory) {
+function WelcomeController($scope, $http, brewFactory) {
     var sayHello = function() {
         console.log("hello!");
     }
@@ -12,10 +12,23 @@ function WelcomeController($scope, brewFactory) {
     // $scope.beers = brewFactory.goodppl;
     
     var getBrews = function() {
-        brewFactory.getBrews();
-        console.log(brewFactory.goodppl);
-        // $scope.beers = brewFactory.goodppl;
+        $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
+            console.log(data.response.brewery.beer_list.items);
+            var beerList = data.response.brewery.beer_list.items;
+        });
+        
+        // brewFactory.getBrews().success(function(data) {
+        //     console.log(data);
+        // });
+        // console.log(brewFactory.getBrews());
+        // for (var i = 0; i < brewFactory.length; i++) {
+            
+        // }
+        // $scope.beers = brewFactory;        
+        // console.log(brewFactory.getBrews().data.response.brewery.beer_list.items);
+        // $scope.beers = brewFactory;
     }
+    
     // $scope.init = brewFactory.goodppl = [];
     $scope.init = getBrews();
 }
--- js/factories.js
+++ js/factories.js
@@ -3,12 +3,10 @@ var factories = angular.module('brewApp.factories', []);
 factories.factory('brewFactory', ['$http', function($http) {
     var f = {};
     
-    f.goodppl = [];
+    // f.goodppl = [];
     
     f.getBrews = function() {
-        $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
-            f.goodppl.push(data.response.brewery.beer_list.items[0].beer);
-        });
+        $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
     }
     return f;
 }])
