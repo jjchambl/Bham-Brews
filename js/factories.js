812bc69dc59345eb1254262d3011d3aad93a53b9
jjchambl <jjchambliss@gmail.com>
2016-04-26 11:42:09 -0500

    panels finish, comment out welcome

--- js/factories.js
+++ js/factories.js
@@ -7,13 +7,14 @@ factories.factory('goodFactory', ['$http', function($http) {
     
     f.getBrews = function() {
         $http.get('https://api.untappd.com/v4/brewery/info/2811/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB');
-        // See welcomectrl.js.17 for the below handler.
+    }
+        // See welcomectrl.js.17 for the handler below.
         // .success(function(data) {
         //     f.brews.push(data.response.brewery.beer_list.items);
         // }, function(err) {
         //     console.log(err);
         // });
-    }
+    
     return f;
 }]);
 
