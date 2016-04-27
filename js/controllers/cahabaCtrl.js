3f41126e0fead4a19b41223a1ca935deb5853bc4
William Rainey <wmrainey89@gmail.com>
2016-04-27 17:29:01 -0500

    CahabaStyleChanges

--- js/controllers/cahabaCtrl.js
+++ js/controllers/cahabaCtrl.js
@@ -10,12 +10,12 @@ CahabaController.$inject = ["$scope", "$http"];
 function CahabaController($scope, $http) {
     // $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyDm0HxIqsKb8xK49IOV8uGIUWcTbCeSs2E";
     
-    var getBrews = function() {
-        $http.get('https://api.untappd.com/v4/brewery/info/30313/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
-            console.log(data.response.brewery.beer_list.items);
-            var beerList = data.response.brewery.beer_list.items;
-            $scope.beers = beerList;
-        })
-    }
-    $scope.init = getBrews (); 
+    // var getBrews = function() {
+    //     $http.get('https://api.untappd.com/v4/brewery/info/30313/GET?client_id=DBC75E152A90C85FFFDD9562522B89649EAF6252&client_secret=778095373400201EB85CE2084B4829C6C454A7DB').success(function(data) {
+    //         console.log(data.response.brewery.beer_list.items);
+    //         var beerList = data.response.brewery.beer_list.items;
+    //         $scope.beers = beerList;
+    //     })
+    // }
+    // $scope.init = getBrews (); 
 }
